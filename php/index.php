954987e691511dee305aaa14dc7b5f638df7b121
SpaceManiac <tad.hardesty@suddenlink.net>
2010-03-14 20:40:45 -0500

    Added other webspace files

---
+++ php/index.php
@@ -0,0 +1,12 @@
+<?php
+require_once '../wombat.php';
+WombatHead('Robot Wants Kitty: Adventure Edition');
+?>
+<h1>Robot Wants Adventure</h1>
+<p>Welcome to the home of <b>Robot Wants Kitty: Adventure Edition</b>!</p>
+
+<?php embed('rwa', 640, 480); ?>
+
+<p>Current build is <b>84</b>. Hard refresh if what's shown is below that!</p>
+
+<?php WombatFoot(); ?>
