23a28794439837fcf75931a92b720a9636c3bb48
SpaceManiac <tad.hardesty@suddenlink.net>
2010-03-16 16:46:04 -0500

    Added missing files

---
+++ php/linkbar.php
@@ -0,0 +1,6 @@
+<a href="/rwa/">Home</a> |
+<a href="http://github.com/SpaceManiac/RobotWantsAdventure">Contribute</a> |
+<a href="/rwa/about.php">About Adventure Edition</a> |
+<a href="/rwa/upload.php">Upload a level!</a> |
+<a href="/rwa/guide.php">Robot ability guide</a>
+<?php if(isset($_SESSION['rwamod'])) { echo ' | <a href="/rwa/mod.php">Mod CP</a>'; } ?>
